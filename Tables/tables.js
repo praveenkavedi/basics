d94ff50aba288a5bdd5fb99fe41ba1489f7eeefd
praveenkavedi <36299100+praveenkavedi@users.noreply.github.com>
2019-01-28 14:07:00 +0530

    Adding Multiplication table program

---
+++ Tables/tables.js
@@ -0,0 +1,5 @@
+function multiplyby() {
+    num1 = document.getElementById("firstnumber").value;
+    num2 = document.getElementById("secondnumber").value;
+    document.getElementById("result").innerHTML = num1 * num2;
+}
